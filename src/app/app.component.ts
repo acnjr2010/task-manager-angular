b6ad5dfcd12c3b4d1cb2fe56afe5c487c99e5649
Junior <nog.junior84@gmail.com.br>
2019-01-01 04:00:25 -0200

    adding model task

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { TaskmanagerFrontendPage } from '../../e2e/app.po';
+import { stringify } from '@angular/core/src/util';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Gerenciador de Tarefas';
+
+  task: Task = {
+    id: 22,
+    title: 'Enviar orçamento para cliente X'
+  }
+
+  task2: Task = new Task(23, 'Enviar orçamento para cliente Y');
+}
+
+export class Task{
+  public id: number;
+  public title: string;
+  
+  constructor(id: number, title: string){
+    this.id = id;
+    this.title = title;
+  }
 }
