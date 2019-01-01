cb857ac425e85dd61f018f9bbe947eae43c8fa01
Junior <nog.junior84@gmail.com.br>
2019-01-01 18:57:42 -0200

    moving task model to the correct place

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { TaskmanagerFrontendPage } from '../../e2e/app.po';
 import { stringify } from '@angular/core/src/util';
 
+import { Task } from './tasks/shared/task.model';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,21 +11,5 @@ import { stringify } from '@angular/core/src/util';
 })
 export class AppComponent {
   title = 'Gerenciador de Tarefas';
-
-  task: Task = {
-    id: 22,
-    title: 'Enviar orçamento para cliente X'
-  }
-
-  task2: Task = new Task(23, 'Enviar orçamento para cliente Y');
 }
 
-export class Task{
-  public id: number;
-  public title: string;
-  
-  constructor(id: number, title: string){
-    this.id = id;
-    this.title = title;
-  }
-}
---
+++ src/app/tasks/tasks.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { Task } from './shared/task.model';
+
+const TASKS: Array<Task> = [
+    { id: 1, title: 'Fazer tarefa 1'},
+    { id: 2, title: 'Fazer tarefa 2'},
+    { id: 3, title: 'Fazer tarefa 3'},
+    { id: 4, title: 'Fazer tarefa 4'},
+    { id: 5, title: 'Fazer tarefa 5'},
+    { id: 6, title: 'Fazer tarefa 6'},
+    { id: 7, title: 'Fazer tarefa 7'},
+    { id: 8, title: 'Fazer tarefa 8'}
+]
+
+@Component({
+    selector: 'tasks',
+    templateUrl: './tasks.component.html'
+})
+
+export class TaskComponent implements OnInit{
+    public tasks;
+
+    public constructor(){
+    }
+
+    public ngOnInit(){
+        this.tasks = TASKS;
+    }
+}
