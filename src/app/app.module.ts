5e21ff3cd5347d70c379ad691b7e80b4a68adb3c
Junior <nog.junior84@gmail.com.br>
2019-03-03 13:10:39 -0300

    adding http to TaskService

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
+// angular imports
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { AppRoutingModule } from 'app/app-routing.module';
 
+// components imports
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { TaskComponent } from './tasks/tasks.component';
 import { TaskDetailComponent } from './tasks/task-detail/task-detail.component';
 
+// services imports
 import { TaskService } from './tasks/shared/task.service'
+
+// modules imports
+import { AppRoutingModule } from 'app/app-routing.module';
 
 @NgModule({
   declarations: [
