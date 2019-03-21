1b9312231bb06f4fdf67cb935c3f60d9a9b4c126
Junior <nog.junior84@gmail.com.br>
2019-03-21 18:01:00 -0300

    adding SignUpFormComponent base template and adjustments

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { TaskComponent } from './tasks/tasks.component';
 import { TaskDetailComponent } from './tasks/task-detail/task-detail.component';
 import { TaskSearchComponent } from './navbar/task-search/task-search.component';
+import { SignUpFormComponent } from './sign-up-form/sign-up-form.component';
 
 // services imports
 import { TaskService } from './tasks/shared/task.service'
@@ -43,6 +44,7 @@ import * as datetimepicker from 'eonasdan-bootstrap-datetimepicker';
     AppComponent,
     DashboardComponent,
     NavbarComponent,
+    SignUpFormComponent,
     TaskComponent,
     TaskDetailComponent,
     TaskSearchComponent
