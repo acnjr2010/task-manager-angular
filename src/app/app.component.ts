a82c9896ed11abddaf246e4323330388b19f8098
Junior <nog.junior84@gmail.com.br>
2019-03-21 22:19:12 -0300

    adding and setting up the Angular2-token

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,21 @@ import { TaskmanagerFrontendPage } from '../../e2e/app.po';
 import { stringify } from '@angular/core/src/util';
 
 import { Task } from './tasks/shared/task.model';
+import { Angular2TokenService } from 'angular2-token';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'Gerenciador de Tarefas';
+
+  public constructor(private tokenService: Angular2TokenService){
+    this.tokenService.init({
+      apiBase: 'http://localhost:3000'
+    })
+  }
 }
 
